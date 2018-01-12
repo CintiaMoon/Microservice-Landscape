ffb1360a16513dd08b52d97aa789d30d3cb1f4f7
Cintia <CintiaMoon@yahoo.com>
2018-01-12 12:28:52 -0300

    Added Feign Support

--- Landscape-User-Service/src/main/java/com/cargain/micro/landscape/user/controller/UserLogController.java
+++ Landscape-User-Service/src/main/java/com/cargain/micro/landscape/user/controller/UserLogController.java
@@ -1,8 +1,23 @@
 package com.cargain.micro.landscape.user.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.cargain.micro.landscape.user.model.UserLog;
+import com.cargain.micro.landscape.user.repository.UserLogRepository;
 
 @RestController
+@RequestMapping("/userLog")
 public class UserLogController {
 
+  @Autowired
+  UserLogRepository userLogRepository;
+  
+  @GetMapping("/all")
+  public List<UserLog> getAll() {
+      return userLogRepository.findAll();
+  }
+  
 }
